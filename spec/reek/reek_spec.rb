21cc6b2b8d72465440e9f591be60f0fd07ca1c7d
MButt <MButt>
2015-12-02 18:17:09 +0000

    Fixed bug with Rspec test of reek

--- spec/reek/reek_spec.rb
+++ spec/reek/reek_spec.rb
@@ -1,6 +1,6 @@
 describe 'reek_code_smells' do
   it 'should pass all code smell commands' do
     reek_output = `reek ./lib`
-    expect(reek_output).to include("0 total warnings")
+    expect(reek_output).to include("m0 total warnings")
   end
 end
